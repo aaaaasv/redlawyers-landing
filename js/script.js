e63237d938fe7f26a597e2af2c367c31a10a1e48
Andrii Aleksandrov <38463115+aaaaasv@users.noreply.github.com>
2021-02-04 14:49:41 +0200

    Add tooltip handling for social icons

--- js/script.js
+++ js/script.js
@@ -1,35 +1,44 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    autoplay:true,
-    autoplayHoverPause: true,
-    checkvisible:false,
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayHoverPause: true,
+  checkvisible: false,
 
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        },
-        1500:{
-            items:5
-        }
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 4
+    },
+    1500: {
+      items: 5
     }
+  }
 })
 
-$('.icon-telegram').on('click', function() {
-  $(this).siblings('input.telegram-copy').select();
+$('.icon-viber').on('click', function() {
+  $(this).siblings('input.viber-copy').select();
   document.execCommand("copy")
   $(this).attr('title', 'Скопійовано').tooltip('_fixTitle').tooltip('show');
 })
 
-$('.icon-telegram').mouseleave(function() {
-  $(this).attr('title', 'Натисніть, щоб скопіювати').tooltip('_fixTitle');
-
+$('.icon-social').mouseleave(function() {
+  if ($(this).attr('title') !== 'Натисніть, щоб скопіювати') {
+    $(this).attr('title', 'Натисніть, щоб скопіювати').tooltip('_fixTitle').tooltip('show');
+  }
+})
+$('.icon-social').hover(function() {
+  $(this).tooltip('show');
+})
+$('.icon-whatsup').on('click', function() {
+  $(this).siblings('input.whatsup-copy').select();
+  document.execCommand("copy")
+  $(this).attr('title', 'Скопійовано').tooltip('_fixTitle').tooltip('show');
 })
